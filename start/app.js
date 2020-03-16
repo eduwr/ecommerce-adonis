799e6d4139fa66081e3e94ca4158c18fc7e3960c
eduwr <eduwric@gmail.com>
2020-03-16 19:21:44 -0300

    install Websocket provider

--- start/app.js
+++ start/app.js
@@ -17,7 +17,8 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
   '@adonisjs/mail/providers/MailProvider',
-  '@adonisjs/validator/providers/ValidatorProvider'
+  '@adonisjs/validator/providers/ValidatorProvider',
+  '@adonisjs/websocket/providers/WsProvider'
 ]
 
 /*
