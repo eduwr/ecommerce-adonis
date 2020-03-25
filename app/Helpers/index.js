8e365a85ba60c4980c746c1304d09201746c1b15
eduwr <eduwric@gmail.com>
2020-03-25 19:10:15 -0300

    add helpers

---
+++ app/Helpers/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const crypto = use('crypto')
+const Helpers = use('Helpers')
+
+/**
+ * Generate random string
+ *
+ * @param { int } lenght - O tamanho da string que você quer retornar
+ * @return { string } - uma string randomica do tamanho de lenght
+ */
+
+const str_random = async (length = 40) => {
+  let string = ''
+  const len = string.length
+
+  if (len < length) {
+    const size = length - len
+    const bytes = await crypto.randomBytes(size)
+    const buffer = Buffer.from(bytes)
+    string += buffer
+      .toString('base64')
+      .replace(/[^a-zA-Z0-0]/g, '')
+      .substr(0, size)
+
+    return string
+  }
+}
+
+module.exports = { str_random }
