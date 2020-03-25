74015a4eebec5d31b8456997c9ff87f755c7935d
eduwr <eduwric@gmail.com>
2020-03-25 19:43:12 -0300

    Add upload manager Helpers

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const crypto = use('crypto')
-// const Helpers = use('Helpers')
+const Helpers = use('Helpers')
 
 /**
  * Generate random string
  *
- * @param { int } lenght - O tamanho da string que você quer retornar
- * @return { string } - uma string randomica do tamanho de lenght
+ * @param { int } lenght - The string lenght
+ * @return { string } - a random string with given lenght
  */
 
 const str_random = async (length = 40) => {
@@ -27,4 +27,55 @@ const str_random = async (length = 40) => {
   }
 }
 
-module.exports = { str_random }
+/**
+ * Move a single file to the specified path or 'public/uploads' by default.
+ * @param { FileJar } file - the file to be managed.
+ * @param { string } path - the destination path.
+ * @return { Object<FileJar> }
+ */
+
+const manage_single_upload = async (file, path = null) => {
+  path = path || Helpers.publicPath('uploads')
+
+  // generate a random name
+  const random_name = await str_random(30)
+  const filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+  // rename the file and move to destination path
+  await file.move(path, {
+    name: filename
+  })
+
+  return file
+}
+
+/**
+ * Move multiple files to the specified path or 'public/uploads' by default.
+ * @param { FileJar } fileJar
+ * @param { string } path - the destination path.
+ * @return { Object }
+ */
+
+const manage_multiple_uploads = async (fileJar, path = null) => {
+  path = path || Helpers.publicPath('uploads')
+  const successes = []
+  const errors = []
+
+  await Promise.all(
+    fileJar.files.map(async file => {
+      const random_name = await str_random(30)
+      const filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+      await file.move(path, { name: filename })
+
+      if (file.moved()) {
+        successes.push(file)
+      } else {
+        errors.push(file.error())
+      }
+    })
+  )
+  return { successes, error }
+}
+
+module.exports = { str_random, manage_single_upload, manage_multiple_uploads }
