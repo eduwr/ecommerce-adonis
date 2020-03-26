4282b910d49487843730fe0a31d346efb5ff440d
eduwr <eduwric@gmail.com>
2020-03-26 06:52:39 -0300

    Coupon Controller

--- app/Controllers/Http/Admin/CouponController.js
+++ app/Controllers/Http/Admin/CouponController.js
@@ -4,6 +4,9 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Coupon = use('App/Models/Coupon')
+const Database = use('Database')
+const CouponService = use('App/Services/Coupon/CouponService')
 /**
  * Resourceful controller for interacting with coupons
  */
@@ -16,20 +19,16 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    * @param {View} ctx.view
+   * @param {object} ctx.pagination
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new coupon.
-   * GET coupons/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index({ request, response, pagination }) {
+    const query = Coupon.query()
+    const code = request.input('code')
+    if (code) {
+      query.where('code', 'LIKE', `%${code}%`)
+    }
+    const coupons = await query.paginate(pagination.page, pagination.limit)
+    return response.send(coupons)
   }
 
   /**
@@ -40,7 +39,64 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
+    /**
+     * 1 - product - pode ser utilizado apenas em produtos específicos
+     * 2 - clients - pode ser utilizado apenas por clientes específicos
+     * 3 - clients and products - pode ser utilizados somente em produtos e clientes específicos
+     * 4 - pode ser utilizar por qualquer cliente em qualquer pedido
+     */
+
+    const trx = Database.beginTransaction()
+
+    const can_use_for = {
+      client: false,
+      product: false
+    }
+
+    try {
+      const couponData = request.only([
+        'code',
+        'discount',
+        'valid_from',
+        'valid_until',
+        'quantity',
+        'type',
+        'recursive'
+      ])
+
+      const { users, products } = request.only(['users', 'products'])
+      const coupon = await Coupon.create(couponData, trx)
+
+      // starts service layer
+      const service = new CouponService(coupon, trx)
+      if (users && users.length > 0) {
+        await service.syncUsers(users)
+        can_use_for.client = true
+      }
+
+      if (products && products.length > 0) {
+        await service.syncProducts(products)
+        can_use_for.product = true
+      }
+
+      if (can_use_for.product && can_use_for.client) {
+        coupon.can_use_for = 'product_client'
+      } else if (can_use_for.product && !can_use_for.client) {
+        coupon.can_use_for = 'product'
+      } else if (!can_use_for.product && can_use_for.client) {
+        coupon.can_use_for = 'client'
+      } else {
+        coupon.can_use_for = 'all'
+      }
+
+      await coupon.save(trx)
+      await trx.commit()
+      return response.status(201).send(coupon)
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({ message: 'Bad Request' })
+    }
   }
 
   /**
@@ -52,19 +108,9 @@ class CouponController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing coupon.
-   * GET coupons/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show({ params: { id }, response }) {
+    const coupon = await Coupon.findOrFail(id)
+    return response.send(coupon)
   }
 
   /**
@@ -75,7 +121,56 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params: { id }, request, response }) {
+    const trx = await Database.beginTransaction()
+    const coupon = await findOrFail(id)
+    const can_use_for = {
+      client: false,
+      user: false
+    }
+    try {
+      const couponData = request.only([
+        'code',
+        'discount',
+        'valid_from',
+        'valid_until',
+        'quantity',
+        'type',
+        'recursive'
+      ])
+
+      coupon.merge(couponData)
+      const { users, products } = request.only(['users', 'products'])
+
+      const service = new CouponService()
+      if (users && users.length > 0) {
+        await service.syncUsers(users)
+        can_use_for.client = true
+      }
+
+      if (products && products.length > 0) {
+        await service.syncProducts(products)
+        can_use_for.product = true
+      }
+
+      if (can_use_for.product && can_use_for.client) {
+        coupon.can_use_for = 'product_client'
+      } else if (can_use_for.product && !can_use_for.client) {
+        coupon.can_use_for = 'product'
+      } else if (!can_use_for.product && can_use_for.client) {
+        coupon.can_use_for = 'client'
+      } else {
+        coupon.can_use_for = 'all'
+      }
+
+      await coupon.save(trx)
+      await trx.commit()
+
+      return response.send(200).send(coupon)
+    } catch (error) {
+      await trx.rollback()
+      return response.status(400).send({ message: 'Bad Request' })
+    }
   }
 
   /**
@@ -86,7 +181,20 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params: { id }, response }) {
+    const trx = await Database.beginTransaction()
+    const coupon = await Coupon.findOrFail(id)
+    try {
+      await coupon.products().detach([], trx)
+      await coupon.orders().detach([], trx)
+      await coupon.users().detach([], trx)
+      await coupon.delete(trx)
+      await trx.commit()
+      return response.status(204).send()
+    } catch (error) {
+      await trx.rollback()
+      return response.status(404).send('Bad Request')
+    }
   }
 }
 
